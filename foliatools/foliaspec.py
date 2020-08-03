0faf82f2e20c57222e563c23acf7dfbb97a6ac8f
Maarten van Gompel <proycon@anaproy.nl>
2020-08-03 18:45:58 +0200

    added annotationtype_layerclass_map for rust

--- foliatools/foliaspec.py
+++ foliatools/foliaspec.py
@@ -693,6 +693,15 @@ def outputblock(block, target, varname, args, indent = ""):
                         s += indent + "    AnnotationType." + element['properties']['annotationtype'] + ':  ' + element['class'] + ' ,\n'
             s += indent + "    AnnotationType.PREDICATE:  SemanticRolesLayer\n"
             s += indent + "}"
+        elif target == 'rust':
+            s += indent + "match " + args[0] + " {\n"
+            for element in elements:
+                if element['class'].endswith('Layer'):
+                    if 'properties' in element and 'xmltag' in element['properties'] and element['properties']['xmltag'] and 'annotationtype' in element['properties']:
+                        s += indent + "    AnnotationType::" + element['properties']['annotationtype'] + ' => Some(ElementType::' + element['class'] + '),\n'
+            s += indent + "    AnnotationType::PREDICATE => Some(SemanticRolesLayer),\n"
+            s += indent + "    _ => None\n"
+            s += indent + "}"
         else:
             raise NotImplementedError("Block " + block + " not implemented for " + target)
     elif block == 'wrefables':
